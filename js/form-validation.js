b17a16912a6d1950a77cc06a0b198bd49f4ac793
Augusto Tobia <augustotobia@gmail.com>
2020-11-04 09:32:33 -0300

    pulished the js code, mostly organisation and good practices

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,16 +1,5 @@
-
-// var input = [document.getElementById('fullName'), document.getElementById('email'), document.getElementById('password'), 
-//     document.getElementById('repeatPassword'), document.getElementById('age'), document.getElementById('phoneNumber'), 
-//     document.getElementById('city'), document.getElementById('direction'), document.getElementById('postalCode'), 
-//     document.getElementById('dni')];
-
-// var patterns = [/[a-zA-Z]+\s[a-zA-Z]+/,/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-// /[a-zA-Z0-9]{8,}/,document.getElementById('password').value,/[18,]/,
-// // /\d{7,}/,/\w{5,}\s\d+/,/[a-zA-Z0-9]{3,}/,/[a-zA-Z0-9]{3,}/,/[0-9]{7,8}/];
-
-
-
-
+var text = ' ';
+// Elements
 var fullName = document.getElementById('fullName'),
 email = document.getElementById('email'),
 password = document.getElementById('password'),
@@ -22,14 +11,12 @@ postalCode = document.getElementById('postalCode'),
 city = document.getElementById('city'),
 dni = document.getElementById('dni');
 
-var submit = document.getElementById('submit'),
-form = document.getElementById('form'),
-autoText = document.getElementById('autoText');
+var form = document.getElementById('form');
 
 //Event Listeners
 fullName.querySelector('input').addEventListener('blur', valFullName);
 email.querySelector('input').addEventListener('blur', valEmail);
-password.querySelector('input').addEventListener('blur',valPassword);
+password.querySelector('input').addEventListener('blur', valPassword);
 repeatPassword.querySelector('input').addEventListener('blur', valRepeatPassword);
 age.querySelector('input').addEventListener('blur', valAge);
 tel.querySelector('input').addEventListener('blur', valTel);
@@ -50,26 +37,27 @@ postalCode.querySelector('input').addEventListener('focus', focusPostalCode );
 dni.querySelector('input').addEventListener('focus', focusDni);
 
 form.addEventListener('submit', submitForm);
-// submit.addEventListener('submit', submitForm)
 
 
 //Function On Blur
-/*Se podria realizar un array con los hijos del nodo Form y un loop cargando los valores de cada input y 
-chequeandolos contra el pattern correspondiente. Si tengo tiempo lo hago*/
+/*It's posible to make just 1 function to validate every pattern using arrays
+This is the prototype*/
 
-// questions = [fullName, email, password, repeatPassword, age, tel, direction, city, postalCode, dni];
-// patterns = [/[a-zA-Z]+\s[a-zA-Z]+/,/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
-// /[a-zA-Z0-9]{8,}/,document.getElementById('password').value,/[18,]/,
-// /\d{7,}/,/\w{5,}\s\d+/,/[a-zA-Z0-9]{3,}/,/[a-zA-Z0-9]{3,}/,/[0-9]{7,8}/
-// ];
+// fullName.querySelector('input').addEventListener('blur', validate(0));
+// email.querySelector('input').addEventListener('blur', validate(1));
+// password.querySelector('input').addEventListener('blur',validate(2));
+// repeatPassword.querySelector('input').addEventListener('blur', validate(3));
+// age.querySelector('input').addEventListener('blur', validate(4));
+// tel.querySelector('input').addEventListener('blur', validate(5));
+// direction.querySelector('input').addEventListener('blur', validate(6));
+// city.querySelector('input').addEventListener('blur', validate(7));
+// postalCode.querySelector('input').addEventListener('blur', validate(8));
+// dni.querySelector('input').addEventListener('blur', validate(9));
 
-// function validate(e){
-//     for (i=0; i<questions.length; i++){
-//         if (patterns[i].test(questions[i].querySelector('input').value) === false ){
-//             console.log(questions[i].querySelector('p'));
-//         }    
-//     }
-//     e.preventDefault()
+// function validate(i){
+//     if (patterns[i].test(questions[i].querySelector('input').value) === false ){
+//         questions[i].querySelector('p').className = 'errorActive';
+//     }    
 // }
 
 function valFullName(){
@@ -77,11 +65,9 @@ function valFullName(){
     patt = /[a-zA-Z]+\s[a-zA-Z]+/,
     exclude = /\d/;
 
-    if (patt.test(val) == true && val.length >= 6 && exclude.test(val) == false){
-        console.log('right');
+    if (patt.test(val) === true && val.length >= 6 && exclude.test(val) == false){
         return true;
     } else {
-        console.log('wrong');
         fullName.querySelector('p').className = 'errorActive';
         return false;
     }
@@ -93,21 +79,16 @@ function valEmail(){
     console.log(value);
     return true;
     } else {
-        console.log('Wrong')
         email.querySelector('p').className = 'errorActive';
         return false;
     }
 }
 function valPassword(){
     var val = password.querySelector('input').value,
-    patt = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/
+    patt = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
     if(patt.test(val) === true){
-        console.log(val)
-        console.log('BIEEEEN')
         return true;
     } else{
-        console.log('error');
-        console.log(val);
         password.querySelector('p').className = 'errorActive';
         return false;
     }
@@ -116,10 +97,8 @@ function valRepeatPassword(){
     var val = repeatPassword.querySelector('input').value,
     patt = document.getElementById('password').querySelector('input').value;
     if (val === patt){
-        console.log('Bien hecho');
         return true;
     } else {    
-        console.log('Maaal');
         repeatPassword.querySelector('p').className = 'errorActive';
         return false;
     }
@@ -127,7 +106,6 @@ function valRepeatPassword(){
 function valAge(){
     var val = age.querySelector('input').value;
     if (val < 18){
-        console.log('Que hace aca pibe');
         age.querySelector('p').className = 'errorActive';
         return false;
     } else {
@@ -137,8 +115,7 @@ function valAge(){
 function valTel(){
     var val = tel.querySelector('input').value,
     patt = /^([0-9]{7,}$)/;
-    if (patt.test(val) == false){
-        console.log('you made a mistake');
+    if (patt.test(val) === false){
         tel.querySelector('p').className = 'errorActive';
         return false;
     } else {
@@ -148,8 +125,7 @@ function valTel(){
 function valDirection(){
     var val = direction.querySelector('input').value,
     patt = /[a-zA-Z]{5,}\s\d+/;
-    if(patt.test(val) == false){
-        console.log('la cagaste');
+    if(patt.test(val) === false){
         direction.querySelector('p').className = 'errorActive';
         return false;
     } else {
@@ -160,8 +136,7 @@ function valDirection(){
 function valCity(){
     var val = city.querySelector('input').value,
     patt = /[a-zA-Z0-9]{3,}/;
-    if(patt.test(val) == false){
-        console.log('mala esa')
+    if(patt.test(val) === false){
         city.querySelector('p').className = 'errorActive';
         return false;
     } else {
@@ -172,8 +147,7 @@ function valCity(){
 function valPostalCode(){
     var val = postalCode.querySelector('input').value,
     patt = /[a-zA-Z0-9]{3,}/;
-    if(patt.test(val) == false){
-        console.log('mala esa')
+    if(patt.test(val) === false){
         postalCode.querySelector('p').className = 'errorActive';
         return false;
     } else {
@@ -184,7 +158,6 @@ function valPostalCode(){
 function valDni(){
     var val = dni.querySelector('input').value;
     if(val.length < 7 || val.length >8){
-        console.log('mala esa')
         dni.querySelector('p').className = 'errorActive';
         return false;
     } else {
@@ -225,39 +198,10 @@ function focusDni(){
 }
 
 //Function Submit
-var submitTxt = '';
-
-
-// function submitForm(e){
-//     var validations = [valFullName(),valEmail(),valPassword(),valRepeatPassword(),valAge(),valTel(),valDirection(),valCity(),valPostalCode(),valDni()],
-//     questions = [fullName, email, password, repeatPassword, age, tel, direction, city, postalCode, dni],
-//     values = [fullName.querySelector('input').value, email.querySelector('input').value, 
-//     password.querySelector('input').value, repeatPassword.querySelector('input').value, age.querySelector('input').value, 
-//     tel.querySelector('input').value, direction.querySelector('input').value, city.querySelector('input').value, 
-//     postalCode.querySelector('input').value, dni.querySelector('input').value];
-//     var count = 0; 
-
-    
-//     for(i = 0; i < validations.length; i++){
-//         if (validations[i] == true){
-//             submitTxt = submitTxt + ' ' + values[i];
-//             count = ++count;
-//         }
-//     }
-//     if (count == 10){
-//         alert(values.join("\n"));
-//     } else {
-//         console.log(count)
-//         alert('error');
-//         console.log(submitTxt)
-//     }
-//     e.preventDefault()
-// }
-text = ' ';
-var validations = [valFullName(),valEmail(),valPassword(),valRepeatPassword(),valAge(),valTel(),valDirection(),valCity(),valPostalCode(),valDni()],
-questions = [fullName, email, password, repeatPassword, age, tel, direction, city, postalCode, dni];
 
 function submitForm(e){
+    var validations = [valFullName(),valEmail(),valPassword(),valRepeatPassword(),valAge(),valTel(),valDirection(),valCity(),valPostalCode(),valDni()],
+    questions = [fullName, email, password, repeatPassword, age, tel, direction, city, postalCode, dni];
     for (i=0; i < questions.length; i++){
         if (validations[i] === true){
         text = text + questions[i].querySelector('label').innerText + ': ' + questions[i].querySelector('input').value + '\n\n';
@@ -269,5 +213,3 @@ function submitForm(e){
     text = '';
     e.preventDefault();
 }
-
-
